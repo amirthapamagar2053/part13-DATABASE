3bca591ba5261cce32a302f49b88ac395094c49f
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-09-08 13:33:14 +0545

    feat:left for query parameters

---
+++ controllers/noteRouter.js
@@ -0,0 +1,80 @@
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
+
+const { Note, User } = require("../models");
+
+const noteFinder = async (req, res, next) => {
+  req.note = await Note.findByPk(req.params.id);
+  next();
+};
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
+
+// WITHOUT THE CONTENT OF USERS
+// router.get("/", async (req, res) => {
+//   const notes = await Note.findAll();
+//   res.json(notes);
+// });
+router.get("/", async (req, res) => {
+  const notes = await Note.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+  });
+  res.json(notes);
+});
+
+router.post("/", tokenExtractor, async (req, res) => {
+  try {
+    // const note = await Note.create(req.body); // Without using the tokenExtractor
+    const note = await Note.create({
+      ...req.body,
+      userId: req.decodedToken.id,
+    });
+    res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.get("/:id", noteFinder, async (req, res) => {
+  if (req.note) {
+    res.json(req.note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete("/:id", noteFinder, async (req, res) => {
+  if (req.note) {
+    await req.note.destroy();
+  }
+  res.status(204).end();
+});
+
+router.put("/:id", noteFinder, async (req, res) => {
+  if (req.note) {
+    req.note.important = req.body.important;
+    await req.note.save();
+    res.json(req.note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,92 +1,27 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+
+const notesRouter = require("./controllers/noteRouter");
+const userRouter = require("./controllers/userRouter");
+const loginRouter = require("./controllers/loginRouter");
+
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+app.use("/api/notes", notesRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
-class Note extends Model {}
-Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: {
-      type: DataTypes.BOOLEAN,
-    },
-    date: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "note",
-  }
-);
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-Note.sync();
-
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.findAll();
-  //   console.log(notes.map((n) => n.toJSON()));
-  console.log(JSON.stringify(notes, null, 2));
-  res.json(notes);
-});
-
-app.get("/api/notes/:id", async (req, res) => {
-  const note = await Note.findByPk(req.params.id);
-  if (note) {
-    console.log(note.toJSON());
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.post("/api/notes", async (req, res) => {
-  try {
-    console.log(req.body);
-    const note = await Note.create(req.body);
-    console.log("The note is", note);
-    return res.json(note);
-  } catch (error) {
-    console.log("The error has been entered");
-    return res.status(400).json({ error });
-  }
-});
-
-app.put("/api/notes/:id", async (req, res) => {
-  const note = await Note.findByPk(req.params.id);
-  if (note) {
-    note.important = req.body.important;
-    await note.save();
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
 // USING THE SEQUALIZE COMMMAND FOR CONNECTING TO THE DATABASE AND GETTING THE CONTENTS FROM THE DATABASE
 
 // const main = async () => {
